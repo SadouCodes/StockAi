401b0fbfd3d554b2d6e8bf2a82b2f8e9f6878d39
SadouCodes <sadousow100@gmail.com>
2023-07-21 02:20:48 -0400

    tensorflow

--- Data/tensorflow_practice.py
+++ Data/tensorflow_practice.py
@@ -3,10 +3,18 @@ import yfinance as yf
 
 print("TensorFlow Version: " + tf.__version__ + "\n")
 
-data = yf.download(['GOOG', 'META', 'AMZN', 'TSLA'], start="2020-01-01", end="2023-06-01", group_by="ticker")
+data = yf.download(['GOOG', 'META', 'AMZN', 'TSLA'], interval="1d", start="2020-01-01", end="2023-06-01", group_by="ticker")
+data1 = yf.download('GOOG', interval="1d", start="2020-01-01", end="2023-06-01", group_by="ticker")
 
-print(data.head())
+#for ticker in data1:
+#    print(ticker)
 
+print(data1.head())
+
+google = yf.Ticker("GOOG")
+
+
+print(google.info)
 
 
 
